1c436486a941267adc603689484979a42e9b8d0b
Raunak Hajela <raunakhajela@gmail.com>
2020-11-01 19:15:04 +0530

    Showing Alert on reaching end of the Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain quizBrain = new QuizBrain();
 
@@ -36,13 +37,28 @@ class _QuizPageState extends State<QuizPage> {
 
     setState(() {
 
-      if(userPickedAnswer == correctAnswer){
-        scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
-      } else {
-        scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+      if(quizBrain.isFinished() == true){
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        //reset the questionNumber,
+        quizBrain.resetQuiz();
+
+        //empty out the scoreKeeper.
+        scoreKeeper = [];
+      } else{
+        //If we've not reached the end, ELSE do the answer checking steps below 👇
+        if(userPickedAnswer == correctAnswer){
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+        } else {
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+        }
+        //The user picked true
+        quizBrain.nextQuestion();
       }
-      //The user picked true
-      quizBrain.nextQuestion();
 
     });
   }
