e6df0e367db36150a3f21258b59c2e8f30ccea50
Raunak Hajela <raunakhajela@gmail.com>
2020-11-01 18:13:13 +0530

    Added methods to manage our quiz
    Created getQuestionText, getCorrectAnswer and nextQuestion methods to get question/answer from the list and navigate between the next questions

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,10 +1,32 @@
 import 'package:quiz_app/question.dart';
 
 class QuizBrain {
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+  int _questionNumber = 0;
+
+  /*Previously in our main.dart we were able to change the correct answer as questionBank
+  object was public, so we are now changing it to private and this will allow us to
+  not change the value. To access the value we will require a getter method*/
+
+  List<Question> _questionBank = [
+    Question(q: 'Sasuke is powerful than Naruto', a: false),
+    Question(q: 'Saukra is weak', a: false),
     Question(q: 'A slug\'s blood is green.', a: true),
     Question(q: 'Naruto is from Uchiha clan.', a: false)
   ];
+
+  void nextQuestion(){
+    if(_questionNumber < _questionBank.length -1){
+      _questionNumber++;
+    }
+    print(_questionNumber);
+    print(_questionBank.length);
+  }
+
+  String getQuestionText(){
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getCorrectAnswer(){
+    return _questionBank[_questionNumber].questionAnswer;
+  }
 }
